8455309df4fa1fab67a870302e4b5c992a550d19
Paul Vatterott <pfvatterott@gmail.com>
2021-05-29 17:26:46 -0600

    formatting changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
   useTransform,
   AnimatePresence
 } from "framer-motion"
+import AboutMe from "./pages/AboutMe"
 
 function App() {
   let startingDragPoint = {}
@@ -18,7 +19,7 @@ function App() {
 
   function processDragInfo(x, y) {
       console.log(x, y, startingDragPoint)
-      if (x < 500) {
+      if ((startingDragPoint.y / 2) > y) {
           console.log('working')
           setAboutMeVisible(true)
           setMainVisible(false)
@@ -27,6 +28,7 @@ function App() {
   return (
     <div className="App">
       <ParticlesBackground>
+
         {/* Navbar */}
         <Navbar bg="dark" variant="dark">
           <Container>
@@ -53,32 +55,36 @@ function App() {
                   animate={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
                 >
-                  <Row className="vertical-align">
-            <Col>
-              <h3>About Me</h3>
-            </Col>
-            <Col>
-              <motion.div>
-                <motion.div
-                  drag
-                  dragConstraints={{ top: -50, left: -50, right: 50, bottom: 50 }}
-                  style={{ x }}
-                  onDragStart={
-                    (event, info) => startingDragPoint = { x: info.point.x, y: info.point.y }
-                  }
-                  onDragEnd={
-                    (event, info) => processDragInfo(info.point.x, info.point.y)
-                  }
-                >
-                  <img className="dragItem" draggable="false" src="./images/clipart1869989.png"></img>
-                  {/* </motion.div> */}
-                </motion.div>
-              </motion.div>
-            </Col>
-            <Col>
-              <h3>Projects</h3>
-            </Col>
-          </Row>
+                <Row className="AboutMeAlign">
+                  <Col>
+                    <h2>About Me</h2>
+                  </Col>
+                </Row>
+                <Row className="vertical-align">
+                  <Col>
+                    <motion.div>
+                      <motion.div
+                        drag
+                        dragConstraints={{ top: -50, left: -50, right: 50, bottom: 50 }}
+                        style={{ x }}
+                        onDragStart={
+                          (event, info) => startingDragPoint = { x: info.point.x, y: info.point.y }
+                        }
+                        onDragEnd={
+                          (event, info) => processDragInfo(info.point.x, info.point.y)
+                        }
+                      >
+                        <img className="dragItem" draggable="false" src="./images/clipart1869989.png"></img>
+                        {/* </motion.div> */}
+                      </motion.div>
+                    </motion.div>
+                  </Col>
+                </Row>
+                <Row className="ContactAlign">
+                  <Col>
+                    <h2>Contact</h2>
+                  </Col>
+                </Row>
                 </motion.div>
               </Container>  
             )}  
@@ -93,17 +99,7 @@ function App() {
                   animate={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
                 >
-                  <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the bulk of
-                      the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
+                  <AboutMe />
                 </motion.div>
               </Container>  
             )}  
